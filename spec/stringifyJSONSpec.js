5b3bf1a2f62d2edd5f231b39ecc810122d07ca79
lramsey <lramsey177@gmail.com>
2014-02-02 20:34:15 -0800

    add stringifiableValues and nonStringifableValues to stringify specs

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,13 +1,15 @@
 // test cases are described in fixtures.js
 describe("stringifyJSON", function(){
   it("should match the result of calling JSON.stringify", function(){
-
+    
+    arrayWithValidElements = stringifiableValues;
     arrayWithValidElements.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
       expect(result).toEqual(expected);
     });
 
+    objectWithInvalidAttributes = nonStringifiableValues;
     objectWithInvalidAttributes.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
